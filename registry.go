5cd04755f4234282a484de6d2168e39bcfc4f7d4
Mohammed Sohail <sohailsameja@gmail.com>
2023-03-28 06:16:12 +0000

    add: custodial proxy to registry map

--- registry.go
+++ registry.go
@@ -9,10 +9,11 @@ import (
 )
 
 const (
-	TrainingVoucher = "TrainingVoucher"
 	AccountIndex    = "AccountIndex"
-	TokenIndex      = "TokenIndex"
+	CustodialProxy  = "CustodialRegistrationProxy"
 	GasFaucet       = "GasFaucet"
+	TrainingVoucher = "TrainingVoucher"
+	TokenIndex      = "TokenIndex"
 )
 
 // RegistryMap auto-loads all well known custodial related smart contract addresses through the CICRegistry pointer contract.
@@ -20,28 +21,31 @@ func (p *Provider) RegistryMap(ctx context.Context, registryAddress common.Addre
 	var (
 		listFunc = w3.MustNewFunc("addressOf(bytes32)", "address")
 
-		trainingVoucherAddress common.Address
 		accountIndexAddress    common.Address
-		tokenIndexAddress      common.Address
+		custodialProxy         common.Address
 		gasFaucetAddress       common.Address
+		tokenIndexAddress      common.Address
+		trainingVoucherAddress common.Address
 	)
 
 	err := p.Client.CallCtx(
 		ctx,
-		eth.CallFunc(listFunc, registryAddress, common.BytesToHash(common.RightPadBytes([]byte(TrainingVoucher), 32))).Returns(&trainingVoucherAddress),
 		eth.CallFunc(listFunc, registryAddress, common.BytesToHash(common.RightPadBytes([]byte(AccountIndex), 32))).Returns(&accountIndexAddress),
-		eth.CallFunc(listFunc, registryAddress, common.BytesToHash(common.RightPadBytes([]byte(TokenIndex), 32))).Returns(&tokenIndexAddress),
+		eth.CallFunc(listFunc, registryAddress, common.BytesToHash(common.RightPadBytes([]byte(CustodialProxy), 32))).Returns(&custodialProxy),
 		eth.CallFunc(listFunc, registryAddress, common.BytesToHash(common.RightPadBytes([]byte(GasFaucet), 32))).Returns(&gasFaucetAddress),
+		eth.CallFunc(listFunc, registryAddress, common.BytesToHash(common.RightPadBytes([]byte(TokenIndex), 32))).Returns(&tokenIndexAddress),
+		eth.CallFunc(listFunc, registryAddress, common.BytesToHash(common.RightPadBytes([]byte(TrainingVoucher), 32))).Returns(&trainingVoucherAddress),
 	)
 	if err != nil {
 		return nil, err
 	}
 
 	registryMap := make(map[string]common.Address)
-	registryMap[TrainingVoucher] = trainingVoucherAddress
+	registryMap[CustodialProxy] = custodialProxy
 	registryMap[AccountIndex] = accountIndexAddress
-	registryMap[TokenIndex] = tokenIndexAddress
 	registryMap[GasFaucet] = gasFaucetAddress
+	registryMap[TokenIndex] = tokenIndexAddress
+	registryMap[TrainingVoucher] = trainingVoucherAddress
 
 	return registryMap, nil
 }
