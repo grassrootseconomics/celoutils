7b9b49724264feb34319611215797d21b51f5dd7
Mohammed Sohail <sohailsameja@gmail.com>
2023-05-28 20:36:42 +0800

    add: SafeGasLimit
    * closes: 12

--- gas.go
+++ gas.go
@@ -5,6 +5,8 @@ import (
 )
 
 var (
+	// SafeGasLimit is set to 1M gas units.
+	SafeGasLimit = 1000000
 	// SafeGasFeeCap is set to 4x the current MinBaseFee of 5 gwei.
 	SafeGasFeeCap = big.NewInt(20000000000)
 	// SafeGasTipCap is set to 10 wei.
