f35b9ed42e2274f074a4266a21d87bf0acb14162
Mohammed Sohail <sohailsameja@gmail.com>
2023-06-08 12:05:16 +0800

    gas: tune params
    * min required per tx is: 0.0035 CELO

--- gas.go
+++ gas.go
@@ -5,10 +5,10 @@ import (
 )
 
 var (
-	// SafeGasLimit is set to 1M gas units.
-	SafeGasLimit = 1000000
-	// SafeGasFeeCap is set to 4x the current MinBaseFee of 5 gwei.
-	SafeGasFeeCap = big.NewInt(20000000000)
-	// SafeGasTipCap is set to 10 wei.
-	SafeGasTipCap = big.NewInt(10)
+	// SafeGasLimit is set to 350k gas units.
+	SafeGasLimit = 350000
+	// SafeGasFeeCap is set to 2x the current MinBaseFee of 5 gwei.
+	SafeGasFeeCap = big.NewInt(10000000000)
+	// SafeGasTipCap is set to 5 wei.
+	SafeGasTipCap = big.NewInt(5)
 )
