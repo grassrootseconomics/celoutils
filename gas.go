9873e8ac0473af1edc67f22bf991b3b6cdaf937a
Mohammed Sohail <sohailsameja@gmail.com>
2024-08-24 13:37:35 +0800

    fix: the register call can eventually exceed 350k gas units

--- gas.go
+++ gas.go
@@ -6,7 +6,7 @@ import (
 
 var (
 	// SafeGasLimit is set to 350k gas units.
-	SafeGasLimit = 350000
+	SafeGasLimit = 550000
 	// SafeGasFeeCap is set to 2x the current MinBaseFee of 5 gwei.
 	SafeGasFeeCap = big.NewInt(10000000000)
 	// SafeGasTipCap is set to 5 wei.
